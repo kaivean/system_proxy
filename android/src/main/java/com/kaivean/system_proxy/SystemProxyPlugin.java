28862bec9c3bc2aed42980ed423504f5ddf3c241
DemoJameson <demojameson@gmail.com>
2021-09-16 16:40:26 +0800

    update to the v2 Android Plugin APIs #3

--- android/src/main/java/com/kaivean/system_proxy/SystemProxyPlugin.java
+++ android/src/main/java/com/kaivean/system_proxy/SystemProxyPlugin.java
@@ -1,54 +1,45 @@
 package com.kaivean.system_proxy;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import java.util.HashMap;
 import java.util.Map;
 import android.net.ProxyInfo;
 
 import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+
+import androidx.annotation.NonNull;
 
 /** SystemProxyPlugin */
-public class SystemProxyPlugin implements MethodCallHandler {
-  private final Registrar registrar;
-  private final ConnectivityManager manager;
+public class SystemProxyPlugin implements FlutterPlugin, MethodCallHandler {
+  private ConnectivityManager manager;
+  private MethodChannel channel;
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "system_proxy");
-    channel.setMethodCallHandler(new SystemProxyPlugin(registrar));
-  }
-
-  private SystemProxyPlugin(Registrar registrar) {
-    this.registrar = registrar;
-    this.manager =
-            (ConnectivityManager)
-                    registrar
-                            .context()
-                            .getApplicationContext()
-                            .getSystemService(Context.CONNECTIVITY_SERVICE);
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "system_proxy");
+    channel.setMethodCallHandler(this);
+    manager = (ConnectivityManager)flutterPluginBinding.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
   }
 
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getProxySettings")) {
       getNetworkType(manager, result);
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 
   private void getNetworkType(ConnectivityManager manager,  Result result) {
